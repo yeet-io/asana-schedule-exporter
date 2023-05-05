3520ad8991e1a89ff251123746d52dc5be22a3e0
Aaron Qian <aq1018@gmail.com>
2023-05-04 19:31:41 -0700

    dynamic port

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ const app = express();
 app.use('/auth', authRoutes);
 app.use('/callback', callbackRoutes);
 
-app.listen(3000, () => {
-  console.log('Server running on http://localhost:3000');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
 });
